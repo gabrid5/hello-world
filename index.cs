4c01ca355162071ef26931d9c0037ccd0900ac98
gabrid5 <166348223+gabrid5@users.noreply.github.com>
2025-01-07 21:39:30 -0300

    Update index.cs

--- index.cs
+++ index.cs
@@ -1,1 +1,9 @@
-Hello World
+/* Hello World */
+
+Console.WriteLine("Hello World");
+
+var names = new[] { "Ana", "Felipe", "Emillia" };
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name}");
+}
