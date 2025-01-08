87c5ec9af8ee0ffd283235b55ff494a304f3510c
gabrid5 <166348223+gabrid5@users.noreply.github.com>
2025-01-08 10:38:58 -0300

    Update index.cs

--- index.cs
+++ index.cs
@@ -2,8 +2,15 @@
 
 Console.WriteLine("Hello World");
 
-var names = new[] { "Ana", "Felipe", "Emillia" };
+var names = new[] { "gabri", "d", "5" };
+string combinedName = "";  // Variável para armazenar a concatenação das palavras
+
 foreach (var name in names)
 {
+    // Adiciona cada nome ao combinedName
+    combinedName += name;  
     Console.WriteLine($"Hello {name}");
 }
+
+// Exibe a combinação das palavras
+Console.WriteLine($"Combined Name: {combinedName}");
